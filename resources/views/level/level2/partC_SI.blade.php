17f580088a925e6b887ac5eeeb9da0f3b48e7536
Elizabeth Castro <elizacast2302@gmail.com>
2023-04-02 00:34:13 -0500

    codigo_nivel_2

---
+++ resources/views/level/level2/partC_SI.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.layout')
+@section('contenido')
+
+
+    PARTE C SI
+
+    <h4>Pregunta 3</h4>
+    <br>
+    <div>{{$quest->question ?? 0}}</div>
+
+
+
+
+
+@endsection
---
+++ resources/views/level/level2/partD_NO.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.layout')
+@section('contenido')
+
+
+    PARTE D NO
+
+    <h4>Pregunta 3</h4>
+    <br>
+    <div>{{$quest->question ?? 0}}</div>
+
+
+
+
+
+@endsection
